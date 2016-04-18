5f02e3cea4f35500f1d2fc93d8220b68850bf517
Donte Wilson <JittNA@aol.com>
2016-04-18 09:49:28 -0400

    Final Commit
    Added Exe

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TestScript : MonoBehaviour
 {
 
     public ADGP125 classObject;
+    public Text Info;
+    
     public string txt;
 
 	// Use this for initialization
@@ -15,6 +18,8 @@ public class TestScript : MonoBehaviour
         txt = classObject.AD125();
         //I have successfully created a test that shows that my dll can be used within usntiy.
         print("ADGP125 DLL to Unity Test: " + txt);
+
+        Info.text = txt;
 	}
 	
 	// Update is called once per frame
